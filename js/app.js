61e7b3986e74bf35f3c68dc0f8f1931fd13e6fe8
alastaraven <razorbladeshape@gmail.com>
2022-07-25 22:16:56 +0300

    +

---
+++ js/app.js
@@ -0,0 +1,12 @@
+$(document).foundation()
+
+// SCROLL HIDING NAV
+var prev = 0;
+var $window = $(window);
+var nav = $('.scrollhide-nav');
+
+$window.on('scroll', function () {
+	var scrollTop = $window.scrollTop();
+	nav.toggleClass('hidden', scrollTop > prev);
+	prev = scrollTop;
+});
